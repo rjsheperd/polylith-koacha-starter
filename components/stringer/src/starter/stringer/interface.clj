743c301698fc6deeeec2fe511b5f4eea1bb5d1c9
RJ Sheperd <rjsheperd@gmail.com>
2024-01-19 16:12:54 -0800

    init commit

---
+++ components/stringer/src/starter/stringer/interface.clj
@@ -0,0 +1,6 @@
+(ns starter.stringer.interface)
+
+(defn my-concat
+  "Concatenate two strings."
+  [s1 s2]
+  (str s1 s2))
---
+++ components/stringer/test/starter/stringer/interface_test.clj
@@ -0,0 +1,9 @@
+(ns starter.stringer.interface-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [starter.stringer.interface :as stringer]))
+
+(deftest concat-test
+  (testing "Concatenating two strings"
+    (let [s1 "Hello "
+          s2 "World!"]
+    (is (= (str s1 s2) (stringer/my-concat s1 s2))))))
